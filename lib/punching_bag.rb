e4a700cf5718dadb35beac120d866b05564a333c
Shen Sat <shensat@shens-mbp.home>
2020-10-27 19:00:28 +0000

    Add train method to PunchingBag, make remaining methods private

--- lib/punching_bag.rb
+++ lib/punching_bag.rb
@@ -2,26 +2,62 @@ require 'io/console'
 require 'timeout'
 
 class PunchingBag
-	attr_reader :target
+	attr_reader :target, :prompt, :punch
 
-	def intro_text
-		puts "Start training by pressing x.\nHit the bag by pressing x again.\nYour target is #{target}"
+	def initialize
+		@prompt = TTY::Prompt.new
+	end
+
+	def train
+		clear_screen
+
+		pick_target
+
+		intro_text
+
+		set_punch
+
+		result
+
+		show_options
+	end
+
+private
+
+	def clear_screen
+		system('clear')
 	end
 
 	def pick_target
 		@target = rand(50..75)
 	end
 
-	def hit?
-		punch = throw_punch
+	def intro_text
+		puts "Start training by pressing x.\nHit the bag by pressing x again.\nYour target is #{target}"
+	end
 
+	def result
+		hit? ? 'Good punch!' : 'You missed the bag!'
+	end
+
+	def show_options
+		choices = { retry: :retry }
+		
+		answer = prompt.select('What would you like to do?', choices)
+
+		train if answer == :retry
+	end
+
+	def set_punch
+		@punch = throw_punch
+	end
+
+	def hit?
 		lower_range = target - 5
 		upper_range = target + 5
 
 		return punch <= upper_range && punch >= lower_range
 	end
-
-private
 
 	def throw_punch(n = 0)
 		begin
--- spec/punching_bag_spec.rb
+++ spec/punching_bag_spec.rb
@@ -9,33 +9,78 @@ describe 'PunchingBag' do
 		allow(punching_bag).to receive(:target).and_return(60)
 
 		
-		expect{ punching_bag.intro_text }.to output(expected_output).to_stdout
+		expect{ punching_bag.send(:intro_text) }.to output(expected_output).to_stdout
 	end
 	
 	it 'picks target' do
 		srand 1
 
-		punching_bag.pick_target
+		punching_bag.send(:pick_target)
 
 		expect(punching_bag.target).to eq(55) 
+	end
+
+	describe '#set_punch' do
+		before { allow(punching_bag).to receive(:throw_punch).and_return(40) }
+
+		it 'sets punch' do
+			punching_bag.send(:set_punch)
+
+			expect(punching_bag.punch).to eq(40)
+		end
 	end
 	
 	describe '#hit?' do
 		before { allow(punching_bag).to receive(:target).and_return(80) }
 		
 		context 'when on target' do
-			before { allow(punching_bag).to receive(:throw_punch).and_return(85) }	
+			before { allow(punching_bag).to receive(:punch).and_return(85) }	
 
 			it 'returns true' do
-				expect(punching_bag.hit?).to eq(true)
+				expect(punching_bag.send(:hit?)).to eq(true)
 			end
 		end
 
 		context 'when off target' do
-			before { allow(punching_bag).to receive(:throw_punch).and_return(74) }
+			before { allow(punching_bag).to receive(:punch).and_return(74) }
 			
 			it 'returns false' do
-				expect(punching_bag.hit?).to eq(false)
+				expect(punching_bag.send(:hit?)).to eq(false)
+			end
+		end
+	end
+
+	describe '#result' do
+		context 'when bag is hit' do
+			before { allow(punching_bag).to receive(:hit?).and_return(true) }
+
+			it 'puts it was hit' do
+				expect(punching_bag.send(:result)).to eq('Good punch!')
+			end
+		end
+
+		context 'when bag is not hit' do
+			before { allow(punching_bag).to receive(:hit?).and_return(false) }
+
+			it 'puts it was not hit' do
+				expect(punching_bag.send(:result)).to eq('You missed the bag!')
+			end
+		end
+	end
+
+	describe '#show_options' do
+		let(:prompt) { double() }
+		before { allow(TTY::Prompt).to receive(:new).and_return(prompt) }
+
+		context 'when user selects retry' do
+			before do 
+				allow(prompt).to receive(:select).and_return(:retry)
+			end
+			
+			it 'retries' do
+				expect(punching_bag).to receive(:train)
+
+				punching_bag.send(:show_options)
 			end
 		end
 	end
